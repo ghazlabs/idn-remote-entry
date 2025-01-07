a5e24884d9ccc3e4f56011a4651214315c47eac9
Riandy R.N <5761331+riandyrn@users.noreply.github.com>
2025-01-07 12:29:32 +0700

    test: add testcases when testing vacancy resolver;

--- internal/driven/resolver/vacancy_test.go
+++ internal/driven/resolver/vacancy_test.go
@@ -2,12 +2,12 @@ package resolver_test
 
 import (
 	"context"
-	"log"
 	"testing"
 
 	"github.com/ghazlabs/idn-remote-entry/internal/driven/resolver"
 	"github.com/go-resty/resty/v2"
 	"github.com/openai/openai-go"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,17 +18,48 @@ func TestResolve(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	vacURL := "https://job-boards.eu.greenhouse.io/invertase/jobs/4492528101"
-	//vacURL := "https://jobs.ashbyhq.com/matter-labs/f6054a2f-ea5d-42ee-a243-8c3fa95018ef"
-	//vacURL := "https://www.ycombinator.com/companies/akiflow/jobs/yMcXc5g-senior-mobile-developer-flutter"
-	vac, err := vacResolver.Resolve(context.Background(), vacURL)
-	require.NoError(t, err)
+	testCases := []struct {
+		Name           string
+		VacancyURL     string
+		ExpVacancyName string
+		ExpCompanyName string
+	}{
+		{
+			Name:           "Greenhouse URL",
+			VacancyURL:     "https://job-boards.eu.greenhouse.io/invertase/jobs/4492528101",
+			ExpVacancyName: "Developer Advocate - Dart & Flutter Products",
+			ExpCompanyName: "Invertase",
+		},
+		{
+			Name:           "AshbyHQ URL",
+			VacancyURL:     "https://jobs.ashbyhq.com/matter-labs/f6054a2f-ea5d-42ee-a243-8c3fa95018ef",
+			ExpVacancyName: "Senior Protocol Engineer",
+			ExpCompanyName: "Matter Labs",
+		},
+		{
+			Name:           "YCombinator URL",
+			VacancyURL:     "https://www.ycombinator.com/companies/akiflow/jobs/yMcXc5g-senior-mobile-developer-flutter",
+			ExpVacancyName: "Senior Mobile Developer - Flutter",
+			ExpCompanyName: "Akiflow",
+		},
+		{
+			Name:           "Micro1 URL",
+			VacancyURL:     "https://jobs.micro1.ai/post/ee6e8b24-ae08-472f-863b-aabcb1b25cef",
+			ExpVacancyName: "AI Engineer (LInE)",
+			ExpCompanyName: "Micro1",
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			vac, err := vacResolver.Resolve(context.Background(), testCase.VacancyURL)
+			require.NoError(t, err)
 
-	log.Printf("Vacancy: %+v", vac)
+			assert.Equal(t, testCase.ExpVacancyName, vac.JobTitle)
+			assert.Equal(t, testCase.ExpCompanyName, vac.CompanyName)
+			assert.NotEmpty(t, vac.ShortDescription)
+			assert.NotEmpty(t, vac.RelevantTags)
+			assert.NotEmpty(t, vac.CompanyLocation)
+		})
+	}
 
-	require.NotEmpty(t, vac.JobTitle)
-	require.Equal(t, "Invertase", vac.CompanyName)
-	require.NotEmpty(t, vac.ShortDescription)
-	require.NotEmpty(t, vac.RelevantTags)
-	// require.NotEmpty(t, vac.CompanyLocation)
 }
