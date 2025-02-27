d26a673f2c5408e04a3c8154503d344f2d035c66
Ilham Syahid S <ilhamsyahids@gmail.com>
2025-02-27 15:07:46 +0300

    test: adjust locator test
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

--- internal/vacancy-worker/driven/resolver/hqloc/locator_test.go
+++ internal/vacancy-worker/driven/resolver/hqloc/locator_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/ghazlabs/idn-remote-entry/internal/shared/core"
 	"github.com/ghazlabs/idn-remote-entry/internal/testutil"
 	"github.com/ghazlabs/idn-remote-entry/internal/vacancy-worker/driven/resolver/hqloc"
 	"github.com/openai/openai-go"
@@ -12,19 +13,35 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type mockStorage struct {
+	companyLocation string
+}
+
+func (m *mockStorage) LookupCompanyLocation(ctx context.Context, companyName string) (string, error) {
+	return m.companyLocation, nil
+}
+
+func (m *mockStorage) Save(ctx context.Context, v core.Vacancy) (*core.VacancyRecord, error) {
+	return nil, nil
+}
+
 func TestLocate(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping test in short mode.")
 	}
 
+	mockStorage := &mockStorage{}
 	locator, err := hqloc.NewLocator(hqloc.LocatorConfig{
+		Storage:       mockStorage,
+		ModelLLM:      openai.ChatModelGPT4o2024_08_06,
 		OpenaAiClient: openai.NewClient(option.WithAPIKey(env.GetString(testutil.EnvKeyTestOpenAiKey))),
 	})
 	require.NoError(t, err)
 
 	testCases := []struct {
-		CompanyName        string
-		ExpCompanyLocation string
+		CompanyName           string
+		CompanyLocFromStorage string
+		ExpCompanyLocation    string
 	}{
 		{
 			CompanyName:        "Automattic",
@@ -35,12 +52,17 @@ func TestLocate(t *testing.T) {
 			ExpCompanyLocation: "Riyadh, Saudi Arabia",
 		},
 		{
-			CompanyName:        "Fingerprint",
-			ExpCompanyLocation: "Chicago, United States",
+			CompanyName:           "Fingerprint",
+			CompanyLocFromStorage: "Chicago, United States",
+			ExpCompanyLocation:    "Chicago, United States",
 		},
 	}
 	for _, testCase := range testCases {
 		t.Run(testCase.CompanyName, func(t *testing.T) {
+			if testCase.CompanyLocFromStorage != "" {
+				mockStorage.companyLocation = testCase.CompanyLocFromStorage
+			}
+
 			loc, err := locator.Locate(context.Background(), testCase.CompanyName)
 			require.NoError(t, err)
 			require.Equal(t, testCase.ExpCompanyLocation, loc)
