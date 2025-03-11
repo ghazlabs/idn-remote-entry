9819311d7b177ee0feea9b606171b4247a17c3c5
Ilham Syahid S <ilhamsyahids@gmail.com>
2025-03-11 09:32:18 +0300

    feat: add validation for required fields in Vacancy model
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

--- internal/shared/core/model.go
+++ internal/shared/core/model.go
@@ -1,14 +1,27 @@
 package core
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+
+	"gopkg.in/validator.v2"
+)
 
 type Vacancy struct {
-	JobTitle         string   `json:"job_title,omitempty"`
-	CompanyName      string   `json:"company_name,omitempty"`
+	JobTitle         string   `json:"job_title" validate:"nonzero"`
+	CompanyName      string   `json:"company_name" validate:"nonzero"`
 	CompanyLocation  string   `json:"company_location,omitempty"`
 	ShortDescription string   `json:"short_description,omitempty"`
 	RelevantTags     []string `json:"relevant_tags,omitempty"`
-	ApplyURL         string   `json:"apply_url,omitempty"`
+	ApplyURL         string   `json:"apply_url" validate:"nonzero"`
+}
+
+func (r Vacancy) Validate() error {
+	err := validator.Validate(r)
+	if err != nil {
+		return fmt.Errorf("invalid vacancy: %w", err)
+	}
+	return nil
 }
 
 type VacancyRecord struct {
