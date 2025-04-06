45f0af7ec4390f6d37f3215b8de78addb3944e99
Ilham Syahid S <ilhamsyahids@gmail.com>
2025-04-06 16:31:48 +0300

    feat: implement server client for vacancy submission
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

---
+++ internal/crawler-worker/driven/server/client.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+	"gopkg.in/validator.v2"
+)
+
+type Server struct {
+	ServerConfig
+}
+
+type ServerConfig struct {
+	HttpClient *resty.Client `validate:"nonnil"`
+	ApiKey     string        `validate:"nonzero"`
+}
+
+func NewClientServer(cfg ServerConfig) (*Server, error) {
+	err := validator.Validate(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("invalid configuration: %w", err)
+	}
+	return &Server{
+		ServerConfig: cfg,
+	}, nil
+}
+
+func (s *Server) SubmitURLVacancy(ctx context.Context, applyURL string) error {
+	payload := map[string]interface{}{
+		"submission_type":  "url",
+		"submission_email": "crawler",
+		"apply_url":        applyURL,
+	}
+	resp, err := s.HttpClient.R().
+		SetHeader("Content-Type", "application/json").
+		SetHeader("X-Api-Key", s.ApiKey).
+		SetBody(payload).
+		Post("/vacancies")
+	if err != nil {
+		return fmt.Errorf("failed to call api to save the vacancy: %w", err)
+	}
+	if resp.IsError() {
+		return fmt.Errorf("failed to save the vacancy: %s", resp.String())
+	}
+
+	return nil
+}
