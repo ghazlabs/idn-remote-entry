d597d14cf59ca5cfce4b171c875edcbcb1122bc2
Ilham Syahid S <ilhamsyahids@gmail.com>
2025-04-10 11:27:50 +0300

    fix: save vacancy on storage separately on bulk request
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

--- internal/server/driven/storage/mysql/mysql.go
+++ internal/server/driven/storage/mysql/mysql.go
@@ -89,9 +89,13 @@ func (s *MySQLStorage) SaveBulkApprovalRequest(ctx context.Context, req shcore.S
 	defer stmt.Close()
 
 	// Save each message ID with the corresponding request data
-	for _, messageID := range messageIDs {
-		data := req.ToJSON()
-		_, err := stmt.ExecContext(ctx, messageID, core.ApprovalStatePending, data)
+	for idx, messageID := range messageIDs {
+		data := shcore.SubmitRequest{
+			SubmissionType:  req.SubmissionType,
+			SubmissionEmail: req.SubmissionEmail,
+			Vacancy:         req.BulkVacancies[idx],
+		}
+		_, err := stmt.ExecContext(ctx, messageID, core.ApprovalStatePending, data.ToJSON())
 		if err != nil {
 			tx.Rollback()
 			return fmt.Errorf("failed to save bulk approval request: %w", err)
