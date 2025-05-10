073ab01bd403d69821262e62ed9fbb34d0e298a8
Ilham Syahid S <ilhamsyahids@gmail.com>
2025-05-10 08:52:43 +0700

    fix: normalize relevant tags in VacancyInfo before returning
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

--- internal/vacancy-worker/driven/resolver/model.go
+++ internal/vacancy-worker/driven/resolver/model.go
@@ -1,6 +1,10 @@
 package resolver
 
-import "github.com/ghazlabs/idn-remote-entry/internal/shared/core"
+import (
+	"strings"
+
+	"github.com/ghazlabs/idn-remote-entry/internal/shared/core"
+)
 
 type VacancyInfo struct {
 	JobTitle         string   `json:"job_title" jsonschema_description:"Title of the job taken from the vacancy description, if not found then it should be empty"`
@@ -11,12 +15,20 @@ type VacancyInfo struct {
 }
 
 func (i VacancyInfo) ToVacancy(url string) *core.Vacancy {
+	tags := make([]string, 0)
+	for _, tag := range i.RelevantTags {
+		tag = strings.TrimSpace(strings.ToLower(tag))
+		if tag != "" {
+			tags = append(tags, tag)
+		}
+	}
+
 	return &core.Vacancy{
 		JobTitle:         i.JobTitle,
 		CompanyName:      i.CompanyName,
 		CompanyLocation:  i.CompanyLocation,
 		ShortDescription: i.ShortDescription,
-		RelevantTags:     i.RelevantTags,
+		RelevantTags:     tags,
 		ApplyURL:         url,
 	}
 }
